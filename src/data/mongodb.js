c85d814af171cda2c1c1656cebdb871f4f590fce
Kasra Fallahi <kasrafa99@gmail.com>
2022-10-10 14:30:06 +0330

    morgan logger added

--- src/data/mongodb.js
+++ src/data/mongodb.js
@@ -1,8 +1,17 @@
 // imports
 import mongoose from 'mongoose';
 
-const dbName = 'todo-list-db';
+// const dbName = 'todo-list-db';
+const dbName = 'test';
 
-mongoose.connect(process.env.MONGODB_URL + '/' + dbName, {
-    useNewUrlParser: true
-});
+(async () => {
+    try {
+        await mongoose.connect(
+            process.env.MONGODB_URL + '/' + dbName
+            , { useNewUrlParser: true });
+        console.log('connected successfuly');
+    }
+    catch (e) {
+        console.log(e);
+    }
+})();
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // imports
 import express from 'express';
+import morgan from 'morgan';
 import './data/mongodb.js';
 import taskRouter from './routes/task.js';
 import taskListRouter from './routes/taskList.js';
@@ -7,8 +8,10 @@ import userRouter from './routes/user.js';
 
 // init express
 const app = express();
-app.use(express.json());
 const port = process.env.PORT;
+
+app.use(express.json());
+app.use(morgan('[:date[web]] :method :url :status - :response-time ms'));
 
 // assign routers
 app.use(taskRouter);
